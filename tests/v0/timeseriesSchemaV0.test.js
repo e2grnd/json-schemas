38faefc4c9972f5ce016669441e3c1ed75f6cef1
Nick Haumann <nhaumann@e2g.com>
2024-02-07 12:14:45 -0500

    Working jest suite

--- tests/v0/timeseriesSchemaV0.test.js
+++ tests/v0/timeseriesSchemaV0.test.js
@@ -1,15 +1,24 @@
-// Assuming the structure you provided and the path to the schema file
-const timeseriesSchema = require('../../v0/timeseries_schema.json');
+const fs = require('fs');
+const path = require('path');
+const Ajv = require('ajv/dist/2020').default;
+const ajv = new Ajv({ strict: false, allErrors: true});
+
+const timeseriesSchema = require('../../v0/input.timeseries.schema.json');
+ajv.getSchema("https://json-schema.org/draft/2020-12/schema")
+const validate_timeseries = ajv.compile(timeseriesSchema);
+
 
 describe('Timeseries data validation', () => {
   it('validates passing data against the schema', () => {
     const passingData = require('../../data/v0/passing_timeseries_v0.json');
-    expect(passingData).toMatchSchema(timeseriesSchema);
+    const isValid = validate_timeseries(passingData);
+    expect(validate_timeseries.errors).toBeNull();
+    expect(isValid).toBeTruthy(); // Assert that the data passes validation
   });
 
   it('validates failing data against the schema', () => {
     const failingData = require('../../data/v0/failing_timeseries_v0.json');
-    // Use .not for expecting the validation to fail
-    expect(failingData).not.toMatchSchema(timeseriesSchema);
+    const isValid = validate_timeseries(failingData);
+    expect(validate_timeseries.errors).toMatchSnapshot(); // Assert that the data fails validation
   });
-});
+})
